4bdcfc1439a1bfb07db8f26777b527ad2309abb5
speccode <1951131+speccode@users.noreply.github.com>
2026-07-28 01:28:14 +0200

    feat: blog listing and migrated Remote Event Storming post

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,16 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("site/css");
   eleventyConfig.addPassthroughCopy({ "site/CNAME": "CNAME" });
   eleventyConfig.addPassthroughCopy({ "site/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({
+    "site/blog/posts/remote-event-storming-assets": "blog/remote-event-storming",
+  });
 
   eleventyConfig.addCollection("post", function (collectionApi) {
     return collectionApi.getFilteredByTag("post").sort((a, b) => b.date - a.date);
+  });
+
+  eleventyConfig.addFilter("readableDate", function (dateObj) {
+    return dateObj.toISOString().slice(0, 10);
   });
 
   return {
